15784c72e866a28564015f06047aaf749db7e122
ida-zrt <ida_zrt@163.com>
2020-08-08 19:08:32 +0800

    depth esitmation

--- datasets/sjtuRoad/tools/utils/depthConversion.py
+++ datasets/sjtuRoad/tools/utils/depthConversion.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import cv2 as cv
 import numpy as np
 
@@ -25,6 +26,13 @@ def getDepthFromCloud(cloudPoints,
 
     depthImg = np.zeros(imgSize)
     depthImg[imgPoints[:, 0], imgPoints[:, 1]] = depth
+    print('depth mean is {} m'.format(np.mean(depth)))
+    print('depth var is {} m2'.format(np.var(depth)))
+    print('depth max is {} m'.format(np.max(depth)))
+    crit = 2 * np.sqrt(np.var(depth)) + np.mean(depth)
+    percent = np.sum(depth < crit) / np.sum(np.ones_like(depth))
+    print('{:.2f} %% depth is below {} m'.format(percent * 100, crit))
+    print('')
     depthImg = (np.transpose(depthImg) * 65536 / maxDepth).astype(np.uint16)
     return depthImg
 
@@ -35,10 +43,11 @@ def generateColoredDepthImg(depthImg):
         np.expand_dims(depthHue.astype(np.uint8), 2)
     v = v / np.max(v) * 50 + 180
     v[v == 180] = 0
-    hsv = np.concatenate([
-        np.expand_dims(depthHue.astype(np.uint8), 2),
-        np.expand_dims(np.ones((depthHue.shape)), 2) * 230, v
-    ], 2).astype(np.uint8)
+    v[depthImg > 23400] = 180
+    s = np.expand_dims(np.ones((depthHue.shape)), 2) * 230
+    s[depthImg > 23400] = 0
+    hsv = np.concatenate([np.expand_dims(depthHue.astype(np.uint8), 2), s, v],
+                         2).astype(np.uint8)
 
     coloredDepthImg = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
     return coloredDepthImg
