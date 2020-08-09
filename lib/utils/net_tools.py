0a6d69a17f8d4bb8f097c4fa36525a083781e783
ida-zrt <ida_zrt@163.com>
2020-08-09 12:29:15 +0800

    custom save name

--- lib/utils/net_tools.py
+++ lib/utils/net_tools.py
@@ -36,7 +36,9 @@ def load_ckpt(args, model, optimizer=None, scheduler=None, val_err=[]):
     """
     if os.path.isfile(args.load_ckpt):
         logger.info("loading checkpoint %s", args.load_ckpt)
-        checkpoint = torch.load(args.load_ckpt, map_location=lambda storage, loc: storage, pickle_module=dill)
+        checkpoint = torch.load(args.load_ckpt,
+                                map_location=lambda storage, loc: storage,
+                                pickle_module=dill)
         model.load_state_dict(checkpoint['model_state_dict'])
         if args.resume:
             args.batchsize = checkpoint['batch_size']
@@ -50,21 +52,33 @@ def load_ckpt(args, model, optimizer=None, scheduler=None, val_err=[]):
         torch.cuda.empty_cache()
 
 
-def save_ckpt(args, step, epoch, model, optimizer, scheduler, val_err={}):
+def save_ckpt(args,
+              step,
+              epoch,
+              model,
+              optimizer,
+              scheduler,
+              val_err={},
+              save_name=None):
     """Save checkpoint"""
     ckpt_dir = os.path.join(cfg.TRAIN.LOG_DIR, 'ckpt')
     if not os.path.exists(ckpt_dir):
         os.makedirs(ckpt_dir)
-    save_name = os.path.join(ckpt_dir, 'epoch%d_step%d.pth' %(epoch, step))
+    if save_name is None:
+        save_name = os.path.join(ckpt_dir,
+                                 'epoch%d_step%d.pth' % (epoch, step))
     if isinstance(model, nn.DataParallel):
         model = model.module
-    torch.save({
-        'step': step,
-        'epoch': epoch,
-        'batch_size': args.batchsize,
-        'scheduler': scheduler.state_dict(),
-        'val_err': val_err,
-        'model_state_dict': model.state_dict(),
-        'optimizer': optimizer.state_dict()},
-        save_name, pickle_module=dill)
+    torch.save(
+        {
+            'step': step,
+            'epoch': epoch,
+            'batch_size': args.batchsize,
+            'scheduler': scheduler.state_dict(),
+            'val_err': val_err,
+            'model_state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict()
+        },
+        save_name,
+        pickle_module=dill)
     logger.info('save model: %s', save_name)
