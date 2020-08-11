e5c327230b27d86645e756b1c1f50e97004b5ff0
ida-zrt <ida_zrt@163.com>
2020-08-11 18:53:15 +0800

    save a random model before start

--- train_nyu_metric.py
+++ train_nyu_metric.py
@@ -153,6 +153,9 @@ if __name__ == '__main__':
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer.optimizer,
                                                   lr_lambda=lr_optim_lambda)
 
+    save_ckpt(train_args, 0, 0, model, optimizer.optimizer, scheduler,
+                  val_err[0], 'random')
+
     # load checkpoint
     if train_args.load_ckpt:
         load_ckpt(train_args, model, optimizer.optimizer, scheduler, val_err)
