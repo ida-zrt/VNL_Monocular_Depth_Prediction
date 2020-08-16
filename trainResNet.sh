e5bc716bf0dcdfc69fc1c3f283326c139e86d380
ida-zrt <ida_zrt@163.com>
2020-08-16 22:31:53 +0800

    train for 30 epoch

--- trainResNet.sh
+++ trainResNet.sh
@@ -12,5 +12,5 @@ python -u train_nyu_metric.py --dataroot ./datasets/sjtuRoad \
                                 --dataset road \
                                 --cfg_file lib/configs/resnext101_32x4d_sjtuRoad_class \
                                 --load_ckpt ../VNL_Weight_Pretrained/kitti_eigen.pth \
-                                --epoch 80
+                                --epoch 30
                                 # --resume \
