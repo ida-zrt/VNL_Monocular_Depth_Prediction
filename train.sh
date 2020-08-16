62728b23593389250acd78680856473b18f90800
ida-zrt <ida_zrt@163.com>
2020-08-16 17:42:06 +0800

    train script

--- train.sh
+++ train.sh
@@ -10,7 +10,7 @@ module load anaconda3/2019.07
 source activate VNL
 python -u train_nyu_metric.py --dataroot ./datasets/sjtuRoad \
                                 --dataset road \
-                                --cfg_file lib/configs/resnext101_32x4d_sjtuRoad_class.yaml \
+                                --cfg_file lib/configs/resnext101_32x4d_sjtuRoad_class \
                                 --load_ckpt ../VNL_Weight_Pretrained/kitti_eigen.pth \
                                 --resume \
                                 --epoch 80
