38c4c6d462dfd19e112c876ed302e1108722dea9
ida-zrt <ida_zrt@163.com>
2020-08-17 17:42:20 +0800

    testScript

--- testAnyImage.sh
+++ testAnyImage.sh
@@ -1,5 +1,15 @@
 # "./lib/configs/resnext101_32x4d_sjtuRoad_class.yaml"
 # "./outputs/Aug16-22-37-14_node3/ckpt/final.pth"
+#!/bin/bash
+#SBATCH -J VNL_Train
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --output=log.%j.out
+#SBATCH --error=log.%j.err
+#SBATCH -t 30:00:00
+#SBATCH --gres=gpu:2
+module load anaconda3/2019.07
+source activate VNL
 python  ./test_any_images.py \
 		--dataroot    ./ \
 		--dataset     any \
