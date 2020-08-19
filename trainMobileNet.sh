292e90f34da20d491cf17283629418c2595c7679
ida-zrt <ida_zrt@163.com>
2020-08-20 02:34:28 +0800

    mobile net train sh

---
+++ trainMobileNet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J VNL_Train
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --output=log.%j.out
+#SBATCH --error=log.%j.err
+#SBATCH -t 30:00:00
+#SBATCH --gres=gpu:2
+module load anaconda3/2019.07
+source activate VNL
+python -u train_nyu_metric.py --dataroot ./datasets/sjtuRoad \
+                                --dataset road \
+                                --cfg_file lib/configs/mobilenet_v2_sjtuRoad_class \
+                                --epoch 50
+                                # --resume \
